6018d423bef9bf96a8648584b7f92feb6bdc5d2e
Barbora Dudová <barbora.dudova98@seznam.cz>
2023-05-15 15:48:44 +0200

    Komentář

--- obsluha.js
+++ obsluha.js
@@ -4,7 +4,7 @@ const seznamPojistencu = new SeznamPojistencu();
 seznamPojistencu.vypisZaznamy();
 
 //localStorage.clear();
-
+ //komentář
 
 //ještě dodat seřazení záznamů + ošetřit vstupy (aby nešlo vyplnit prázdné pole atd.)
 
